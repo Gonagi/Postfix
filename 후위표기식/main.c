e69427d0ba5ad599c5a150b4bd6af1278f247596
Gonagi <kkh10002@naver.com>
2022-04-07 15:11:47 +0900

    exit 입력시 종료 Infix 입력시 	Infix를 Postfix으로 변환한다 	Postfix으로 식을 계산하여 결과를 도출한다.

--- 후위표기식/main.c
+++ 후위표기식/main.c
@@ -4,6 +4,28 @@
 
 int main()
 {
+	char command[BUFFER_SIZE] = "\0";
+	char Infix[BUFFER_SIZE] = "\0";
+	char Postfix[BUFFER_SIZE] = "\0";
+
+	while (1) {
+		printf("식을 입력하세요 : \n");
+		printf("중위표기식 : ");
+
+		if (read_line(stdin, command, BUFFER_SIZE) > 0) {
+			if (strcmp(command, "exit") == 0) {
+				printf("종료\n");
+				break;
+			}
+
+			printf("\nInfix : %s", strcpy(Infix, command));
+			printf("\nPostfix : %s", strcpy(Postfix, Make_postfix(Infix)));
+			printf("\nResult : %d\n", Eval(Postfix));
+		}
+		else {
+			printf("다시입력해주세요.\n");
+		}
+	}
 
 	return 0;
 }
