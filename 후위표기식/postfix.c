b96cf622c45ab42bbf2a23ffc090322136851d68
Gonagi <kkh10002@naver.com>
2022-04-07 15:59:47 +0900

    Make_postfix함수 구현 Prec함수 구현(연산자 우선선위 확인함수)

--- 후위표기식/postfix.c
+++ 후위표기식/postfix.c
@@ -1,6 +1,44 @@
 #include "postfix.h"
 #include "stack.h"
 
+void Make_operator_postfix(char* Postfix, char* token)
+{
+	if (Is_empty(Operator_stack))	// 스택이 비었을시
+		Push(Operator_stack, token[0]);
+	else {
+		if(Prec(Peek(Operator_stack))
+	}
+}
+
+int Prec(char Operator)
+{
+	switch (Operator) {
+	case '+' : case '-' :
+		return 1;
+	case '*' : case '/' :
+		return 2;
+	}
+}
+
+char* Make_postfix(char* Infix)
+{
+	char Postfix[BUFFER_SIZE] = "\0";
+	char* token = strtok(Infix, " ");
+
+	while (token != NULL) {
+		if ('0' <= token[0] && token[0] <= '9') {	// 숫자일때
+			strcat(Postfix, token);
+			strcat(Postfix, " ");
+		}
+		else {	// 문자일때
+			Make_operator_postfix(Postfix, token);
+		}
+		token = strtok(NULL, " ");
+	}
+
+	return Postfix;
+}
+
 int read_line(FILE* fp, char str[], int num)
 {
 	int ch, i = 0;
--- 후위표기식/postfix.h
+++ 후위표기식/postfix.h
@@ -1,5 +1,17 @@
-#ifndef POSTFIX_H
-#define POSTFIX_H
+#ifndef __POSTFIX_H__ 
+#define __POSTFIX_H__ 
 
 #endif
+#include <stdio.h>
+#include <stdlib.h>
 #include "stack.h"
+#define BUFFER_SIZE 100
+
+static Operator[] = "+-*/";
+Stack Operator_stack;
+
+void Make_operator_postfix(char*, char*);
+int Prec(char);
+char* Make_postfix(char*);
+int Eval(char*);
+int read_line(FILE*, char[], int);
